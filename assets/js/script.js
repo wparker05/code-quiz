5703525f5f44a1992d1f5036467dc00b2683deda
Wesley Rogers <wprogers86@gmail.com>
2021-11-28 18:49:54 -0500

    added html css and js page

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+var questions = [
+{
+question: "Arrays in JavaScript can be used to store _______.",
+choice1: "numbers and strings",
+choice2: "other arrays",
+choice3: "booleans",
+choice4: "all of the above",
+answer: "all of the above"
+},
+{question: "String values must be enclosed within _____ when being assigned to variables",
+choice1: "commas",
+choice2: "curley brackets",
+choice3: "quotes",
+choice4: "parentheses",
+answer: "quotes"
+},
+{
+question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+choice1: "JavaScript",
+choice2: "terminal/bash",
+choice3: "for loops",
+choice4: "console log",
+answer: "terminal/bash"
+}
+];
+
+var timer = 60;
