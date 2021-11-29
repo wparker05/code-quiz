8c5518302b154400b77c5c141d9ca7cd491fe15b
Wesley Rogers <wprogers86@gmail.com>
2021-11-29 17:21:58 -0500

    updated questions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,94 +1,119 @@
 var questions = [
 {
 question: "Arrays in JavaScript can be used to store _______.",
-choice1: "numbers and strings",
-choice2: "other arrays",
-choice3: "booleans",
-choice4: "all of the above",
+choices: ["numbers and strings", "other arrays","booleans","all of the above"],
 answer: "all of the above"
 },
 {question: "String values must be enclosed within _____ when being assigned to variables",
-choice1: "commas",
-choice2: "curley brackets",
-choice3: "quotes",
-choice4: "parentheses",
+choices: ["commas","curley brackets","quotes", "parentheses"],
 answer: "quotes"
 },
 {
 question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
-choice1: "JavaScript",
-choice2: "terminal/bash",
-choice3: "for loops",
-choice4: "console log",
+choices: ["JavaScript", "terminal/bash","for loops", "console log"],
 answer: "JavaScript"
 }
 ];
 
 var timer = 60;
-
+var x = 0;
 var timeEl = document.querySelector('#timer');
 var btnEl = document.querySelector('#start-btn');
 var firstSlide = document.querySelector('#start-quiz');
 var questionSlide = document.querySelector('#questions');
+var score = document.querySelector("#score");
+
 timeEl.textContent = "Time: " + timer;
 
 
-var startQuiz = () => {
+
+
+var startQuiz = function(){
 
     var quizTimer = setInterval(function() {
         timer--;
         timeEl.textContent = "Time: " + timer;
+
+        if(x === questions.length - 1){
+             clearInterval(quizTimer);
+            
+        }
+
         if(timer === 0){
             clearInterval(quizTimer);
-        }
+        } 
 
        },1000);
 
 
     firstSlide.classList.add('start-quiz');
-    questionSlide.style.display = "block";
+    questionSlide.style.display = "flex";
 }
 
 
-var x = 0;
-var takeQuiz = () => { 
-        var qtn = document.querySelector('#qtn');
+
+var takeQuiz = function() { 
+
+        var qtn = document.createElement('p');
         qtn.textContent = questions[x].question;
-        var choice1 = document.querySelector('#choice1');
-        choice1.textContent = questions[x].choice1;
-        var choice2 = document.querySelector('#choice2');
-        choice2.textContent = questions[x].choice2;
-        var choice3 = document.querySelector('#choice3');
-        choice3.textContent = questions[x].choice3;
-        var choice4 = document.querySelector('#choice4');
-        choice4.textContent = questions[x].choice4;      
-        
+        qtn.className = "para";
+        questionSlide.appendChild(qtn);
+        var num = document.createElement('ol');
+        qtn.appendChild(num);
+
+            for (var choice of questions[x].choices){
+                var opt = document.createElement('li');
+                opt.textContent = choice;
+                num.appendChild(opt);
+            }
+      
 }
-
-
-var selectChoice = document.querySelectorAll('.choices');
-        var selectArray = Array.from(selectChoice);
-        selectArray.forEach(function(arr)
-        {
-            arr.addEventListener('click', function(){
-                    if(x < questions.length) {
-                    
-                        if(this.innerHTML === questions[x].answer){
-                            console.log("correct");
-                            x++;
-                            takeQuiz();
-                        } else {
-                            console.log("wrong");
-                            x++;
-                            takeQuiz();
-                        }
-                    } else {
-                            questionSlide.style.display = "none";
-                    }
-            });
-        });
 
 takeQuiz();
 
+var selectChoice = function(event) {
+    
+    if(event.target.matches('li')){
+        var selected = event.target.innerHTML;
+        if(selected === questions[x].answer){
+            console.log("correct")
+            var p = document.querySelector('para');
+            p.style.display= 'none';
+            takeQuiz();
+        } else {
+            console.log('incorrect');
+            timer -= 10;
+        }
+        
+        if( x === questions.length -1){
+            questionSlide.style.display = "none";
+            score.style.display = "flex";
+            
+            
+        }
+            
+        x++;
+        var removeEl = document.querySelector('p');
+        removeEl.remove();
+        takeQuiz();
+        
+    }
+    
+}
+
+var scoreSlide = function(){
+    var scoreTitle = document.createElement('h2');
+    scoreTitle.textContent = 'Score';
+    score.appendChild(scoreTitle);
+    var getScore = document.createElement('p');
+    getScore.textContent = "Your score is " ;
+    score.appendChild(getScore);
+}
+
+scoreSlide();
+
+
 btnEl.addEventListener('click', startQuiz);
+questionSlide.addEventListener('click', selectChoice);
+
 
